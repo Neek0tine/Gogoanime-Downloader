fa41e102d6ec8024c75d27fdf16be65c1ce738bb
Neekotine <77283651+Neek0tine@users.noreply.github.com>
2021-09-26 17:29:30 +0700

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Gogoanime-Downloader',
+    version='0.1',
+    packages=[''],
+    url='https://github.com/Neek0tine/Gogoanime-Downloader',
+    license='GNU General Public License v3.0',
+    author='Neekotine',
+    author_email='nicholas.juan.kalvin-2020@unair.ac.id',
+    description='Automatic batch downloader for Gogoanime website'
+)
